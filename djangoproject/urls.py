968f5f6c10a8638c56b25be56db3eddb2c2eaa3b
nibinpsan <nibinpsan@gmail.com>
2018-10-29 14:30:35 -0400

    added new URLS and test messages

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.conf.urls import url, include
 
 urlpatterns = [
-    # url(r'^$', include('post.urls')),
+    url(r'^$', include('post.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^post/', include('post.urls')),
 
