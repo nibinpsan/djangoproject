10e83cfdd02272287b8a53171802422cd7d20dbc
nibinpsan <nibinpsan@gmail.com>
2018-10-29 11:31:08 -0400

    added mysql databse information

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoproject',
+        'USER': 'nibinpsan',
+        'PASSWORD': 'dhanya123',
+        'HOST': '192.168.1.197',
+        'PORT': '3306'
     }
 }
 
