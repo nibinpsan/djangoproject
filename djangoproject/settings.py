1396ac881f2171e7d1520367b38709eb5d2b1110
nibinpsan <nibinpsan@gmail.com>
2018-10-29 14:25:30 -0400

    added new URLS and test messages

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'djangoproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'/app/djangoproject/post/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
